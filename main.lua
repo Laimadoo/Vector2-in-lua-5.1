99a98ece8e6a938f83e7970b026196adcd0a9415
Laimadoo <72653939+Laimadoo@users.noreply.github.com>
2024-06-06 21:46:39 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,37 +1,57 @@
-local json = require("json")
 local Vector2 = require("Vector2")
 
 -- creating a vector
 local a = Vector2(3, 4)
 local b = Vector2(-2, 4)
 
+-- tostring
+print(a, b)
+
 -- addition
 local c = a + b
-print(json.encode(c))
+print(c)
 
 -- subtraction
 local c = b - a
-print(json.encode(c))
+print(c)
 
 -- multiplication
 local c = 3 * b
-print(json.encode(c))
+print(c)
 
 -- division
 local c = a / 2
-print(json.encode(c))
+print(c)
 
--- lenght
-print(c:magnitude())
+-- power
+local c = a^2
+print(c)
+
+-- unary minus
+local c = -b
+print(c)
 
 -- normalization
 c:normalized()
-print(json.encode(c))
+print(c)
+
+-- rotation
+c = b:rotation(math.rad(45))
+print(c)
+
+-- equal
+print(a == b)
+
+-- lenght
+print(a:magnitude())
+
+-- angle
+print(a:angle())
 
 -- scalar product
 local c = a:dot(b)
-print(json.encode(c))
+print(c)
 
 -- vector product
 local c = a:cross(b)
-print(json.encode(c))
+print(c)
